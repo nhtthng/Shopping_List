51d7288f27cce1cb5a404c6256727876182d8d1f
nhat thang <nhtthng2812@gmail.com>
2025-02-26 22:12:41 +0700

    add button to a form

--- lib/widget/new_item.dart
+++ lib/widget/new_item.dart
@@ -62,6 +62,14 @@ class _NewItemState extends State<NewItem> {
                     ], onChanged: (value){}),
                   )
                 ],
+              ),
+              const SizedBox(height: 12,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: const Text('Reset')),
+                  ElevatedButton(onPressed: (){}, child: const Text('Add Item'))
+                ],
               )
             ],
           ),
